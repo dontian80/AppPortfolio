7ca525ca33aa912bec00838d846e998e507709fb
Don Tian <dontian@Dons-MacBook-Pro.local>
2016-11-27 04:26:55 -0800

    Initial commit

---
+++ app/src/main/java/com/dontian/appportfolio/MainActivity.java
@@ -0,0 +1,29 @@
+package com.dontian.appportfolio;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void handleAppButton(View view) {
+        displayToast("This button will launch " + ((TextView)view).getText() + " app");
+    }
+
+    void displayToast(String text) {
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+}
